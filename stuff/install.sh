723d22859019dc1f5730d674e6f7a296192f01d5
atomofiron <atomofiron@gmail.com>
2023-07-03 03:37:37 +0400

    update install script to fix PATH on Debian-based systems

--- stuff/install.sh
+++ stuff/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+println() {
+	printf "$1\n"
+}
+
 err() {
     print "$1"
     exit 1
@@ -29,17 +33,53 @@ if ! [ -d $HOME ]; then
 	err 'Home directory not found'
 fi
 
-dir=$HOME'/.local/bin/'
+local_bin='.local/bin'
+home_local_bin=$HOME'/'$local_bin
+env_file='$HOME/.local/env'
+env_file_path=$HOME'/.local/env'
 
-if [ -f $dir'/green-pain' ]; then
+if [ -f $home_local_bin'/green-pain' ]; then
 	action='updating'
 else
 	action='installation'
 fi
 
-mkdir -p $dir
-ensure cd $dir
-printf "downloading...\n"
+mkdir -p $home_local_bin
+ensure cd $home_local_bin
+println "downloading..."
 ensure curl -X GET -sSfL https://github.com/Atomofiron/green-pain/releases/download/release-0.1.0/green-pain -o green-pain
 ensure chmod u+x green-pain
+
+notice_user=false
+
+case ":${PATH}:" in
+    *:"$home_local_bin":*)
+        ;;
+    *)
+		env_case="
+case \":\${PATH}:\" in
+    *:\"\$HOME/$local_bin\":*)
+        ;;
+    *)
+		export PATH=\$PATH:\$HOME/$local_bin
+        ;;
+esac
+"
+		printf "$env_case" > $env_file_path
+		if check_cmd bash; then
+			printf ". $env_file\n" >> ~/.bashrc
+			println '.bashrc done'
+		fi
+		if check_cmd zsh; then
+			printf ". $env_file\n" >> ~/.zshrc
+			println '.zshrc done'
+		fi
+		notice_user=true
+        ;;
+esac
+
 printf "%s succeed, run \33[1msudo green-pain\33[0m\n" $action
+if $notice_user; then
+	println '... however, first of all to configure your current shell, run:'
+	println "\33[1msource "$env_file"\33[0m"
+fi
