5fedbe513b5eb1af03f5519ef1e27b27271c9df2
atomofiron <atomofiron@gmail.com>
2023-09-09 01:24:30 +0400

    fix lss

--- src/core/lss.rs
+++ src/core/lss.rs
@@ -1,4 +1,4 @@
-use crate::core::ext::{OutputExt, StringVec, VecExt};
+use crate::core::ext::{OutputExt, StringVec};
 use crate::core::selector::{resolve_device, run_adb_with_device};
 use crate::core::strings::SCREENSHOTS_NOT_FOUND;
 use crate::core::util::SHELL;
@@ -6,24 +6,20 @@ use std::process::exit;
 
 const PICTURES_SCREENSHOTS: &str = "/sdcard/Pictures/Screenshots/";
 const DCIM_SCREENSHOTS: &str = "/sdcard/DCIM/Screenshots/";
+const DESKTOP_SCREENSHOTS: &str = "/Pictures/Screenshots/";
+const EXISTS: &str = "exists";
 
 pub fn pull_screenshots(count: usize) {
     let device = resolve_device();
-
-    let mut path = PICTURES_SCREENSHOTS;
-    let mut check_args = vec![SHELL, "file", path].to_string_vec();
-    let mut exists = run_adb_with_device(&device, check_args.clone())
-        .status
-        .success();
-    if !exists {
-        path = DCIM_SCREENSHOTS;
-        let last_index = check_args.last_index();
-        check_args[last_index] = path.to_string();
-        exists = run_adb_with_device(&device, check_args.clone())
-            .status
-            .success();
+    let mut path = DCIM_SCREENSHOTS;
+    let mut check_args = vec![SHELL, "test", "-d", path, "&&", "echo", EXISTS].to_string_vec();
+    let mut stdout = run_adb_with_device(&device, check_args.clone()).stdout();
+    if stdout != EXISTS {
+        path = PICTURES_SCREENSHOTS;
+        check_args[3] = PICTURES_SCREENSHOTS.to_string();
+        stdout = run_adb_with_device(&device, check_args).stdout();
     }
-    if !exists {
+    if stdout != EXISTS {
         SCREENSHOTS_NOT_FOUND.print();
         exit(1);
     }
@@ -37,8 +33,8 @@ pub fn pull_screenshots(count: usize) {
             pull_args.push(format!("{path}{line}"));
         }
         #[allow(deprecated)] // todo replace with a crate
-            let mut dst = std::env::home_dir().unwrap().to_str().unwrap().to_string();
-        dst = format!("{dst}/Pictures/Screenshots/");
+        let mut dst = std::env::home_dir().unwrap().to_str().unwrap().to_string();
+        dst = format!("{dst}{DESKTOP_SCREENSHOTS}");
         std::fs::create_dir_all(dst.clone()).unwrap();
         pull_args.push(dst);
         let output = run_adb_with_device(&device, pull_args);
@@ -47,5 +43,5 @@ pub fn pull_screenshots(count: usize) {
     } else {
         println!("{}", output.stderr());
         exit(output.code());
-    }
+    };
 }
