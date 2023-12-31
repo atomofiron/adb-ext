edcf72823ef456cf44b7f3b6462f800c3b155411
atomofiron <atomofiron@gmail.com>
2023-12-31 20:12:15 +0100

    ensure adb-ext deploy

--- stuff/install.sh
+++ stuff/install.sh
@@ -22,5 +22,5 @@ fi
 
 ensure curl -X GET -sSfL https://github.com/Atomofiron/adb-ext/releases/latest/download/$variant -o adb-ext
 ensure chmod u+x adb-ext
-./adb-ext deploy
+ensure ./adb-ext deploy
 rm $0
