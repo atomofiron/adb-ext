6355dd0ae20b44ad7e720c6ac23136ca102bef2b
atomofiron <atomofiron@gmail.com>
2026-01-28 03:15:09 +0100

    add aliases for 'steal' and 'run'

--- src/core/updater.rs
+++ src/core/updater.rs
@@ -79,7 +79,7 @@ pub fn deploy() -> ExitCode {
     let src = env::args().nth(0).unwrap();
     fs::copy(src, &bin_path).unwrap();
     env::set_current_dir(&bin_dir).unwrap();
-    for link in [ADB, LSS, MSS, SHOT, LSC, MSC, REC, RECORD, BOUNDS, TAPS, POINTER, PORT, LAND, FPORT, FLAND, ACCEL, NOACCEL] {
+    for link in [ADB, LSS, MSS, SHOT, LSC, MSC, REC, RECORD, BOUNDS, TAPS, POINTER, PORT, LAND, FPORT, FLAND, ACCEL, NOACCEL, STEAL, RUN] {
         let _ = remove_link(link);
         make_link(link).unwrap_or_else(|e|
             println!("{SYMLINK_FAIL}{link} ({e})")
